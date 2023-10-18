1086e04b407d14e5ee60cc1b9c954bd4e253f3b0
Johannes <95216349+JojoL94@users.noreply.github.com>
2023-10-18 15:52:34 +0200

    zweite Version der Kamerasteuerung implementiert

--- Assets/KameraSteuerung.cs
+++ Assets/KameraSteuerung.cs
@@ -2,23 +2,43 @@ using UnityEngine;
 
 public class KameraSteuerung : MonoBehaviour
 {
-    public float rotationsgeschwindigkeit = 3.0f;
-    public float bewegungsgeschwindigkeit = 3.0f;
-    public float scrollgeschwindigkeit = 3.0f;
+    [SerializeField] float kameraRotationsgeschwindigkeit = 3.0f;
+    [SerializeField] float kameraBewegungsgeschwindigkeit = 3.0f;
+    private bool isRotating;
+    
+    [SerializeField] float kameraZoomgeschwindigkeit = 5.0f;
+    [SerializeField] float kameraMinZoom = 10f; // Minimale Feldgröße für den Zoom
+    [SerializeField] float kameraMaxZoom = 60f; // Maximale Feldgröße für den Zoom
+
 
     void Update()
     {
-        // Kamerarotation mit Mausbewegung
-        float horizontalRotation = Input.GetAxis("Mouse X") * rotationsgeschwindigkeit;
-        transform.Rotate(0, horizontalRotation, 0);
+        // Kamerarotation mit rechter Maustaste und Mausbewegung
+        if (Input.GetMouseButtonDown(1))
+        {
+            isRotating = true;
+        }
+        else if (Input.GetMouseButtonUp(1))
+        {
+            isRotating = false;
+        }
 
+        if (isRotating)
+        {
+            // Kamerarotation mit Mausbewegung
+            float horizontalRotation = Input.GetAxis("Mouse X") * kameraRotationsgeschwindigkeit;
+            float verticalRotation = Input.GetAxis("Mouse Y") * kameraRotationsgeschwindigkeit * -1;
+            transform.Rotate(0, horizontalRotation, 0);
+            transform.GetChild(0).Rotate(verticalRotation, 0, 0);
+        }
+        
         // Kamerabewegung mit den Tasten W, A, S, D
-        float horizontalBewegung = Input.GetAxis("Horizontal") * bewegungsgeschwindigkeit * Time.deltaTime;
-        float vertikaleBewegung = Input.GetAxis("Vertical") * bewegungsgeschwindigkeit * Time.deltaTime;
+        float horizontalBewegung = Input.GetAxis("Horizontal") * kameraBewegungsgeschwindigkeit * Time.deltaTime;
+        float vertikaleBewegung = Input.GetAxis("Vertical") * kameraBewegungsgeschwindigkeit * Time.deltaTime;
         transform.Translate(new Vector3(horizontalBewegung, 0, vertikaleBewegung));
 
-        // Kamerahöhe mit dem Mausrad ändern
-        float scroll = Input.GetAxis("Mouse ScrollWheel") * scrollgeschwindigkeit;
-        transform.Translate(0, scroll, 0);
+        // Kamerazoom mit dem Mausrad ändern
+        float zoom = Input.GetAxis("Mouse ScrollWheel") * kameraZoomgeschwindigkeit;
+        Camera.main.fieldOfView = Mathf.Clamp(Camera.main.fieldOfView - zoom, kameraMinZoom, kameraMaxZoom);
     }
 }
