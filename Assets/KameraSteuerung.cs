114297b5f012ecdf007a09834c576ed5d330f3bb
Johannes <95216349+JojoL94@users.noreply.github.com>
2023-10-18 16:03:59 +0200

    Finale Version der Kamerasteuerung implementiert
    - WASD
    - Zoomen durch scrollen (nicht unter den Boden)
    - Umgucken / rotieren (Taste rechte Maustaste)
    - Höhe durch STRG nach unten verstellen und mit Shift nach oben verstellen

--- Assets/KameraSteuerung.cs
+++ Assets/KameraSteuerung.cs
@@ -9,7 +9,9 @@ public class KameraSteuerung : MonoBehaviour
     [SerializeField] float kameraZoomgeschwindigkeit = 5.0f;
     [SerializeField] float kameraMinZoom = 10f; // Minimale Feldgröße für den Zoom
     [SerializeField] float kameraMaxZoom = 60f; // Maximale Feldgröße für den Zoom
-
+    [SerializeField] float kameraHöhenverstellgeschwindigkeit = 2.0f;
+    [SerializeField] float minKameraHöhe = 10f;
+    [SerializeField] float maxKameraHöhe = 20f;
 
     void Update()
     {
@@ -40,5 +42,22 @@ public class KameraSteuerung : MonoBehaviour
         // Kamerazoom mit dem Mausrad ändern
         float zoom = Input.GetAxis("Mouse ScrollWheel") * kameraZoomgeschwindigkeit;
         Camera.main.fieldOfView = Mathf.Clamp(Camera.main.fieldOfView - zoom, kameraMinZoom, kameraMaxZoom);
+        
+        // Kamerahöhe mit den Tasten STRG (nach unten) und Shift (nach oben) ändern
+        float höhenverstellung = 0f;
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            höhenverstellung = -kameraHöhenverstellgeschwindigkeit * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.LeftShift))
+        {
+            höhenverstellung = kameraHöhenverstellgeschwindigkeit * Time.deltaTime;
+        }
+
+        // Anwenden der Höhenverstellung und sicherstellen, dass die Höhe im zulässigen Bereich bleibt
+        transform.Translate(0, höhenverstellung, 0);
+        Vector3 newPosition = transform.position;
+        newPosition.y = Mathf.Clamp(newPosition.y, minKameraHöhe, maxKameraHöhe);
+        transform.position = newPosition;
     }
 }
