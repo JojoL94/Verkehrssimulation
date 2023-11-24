e06abb0e0261d54393bba3c0e3360aa1df80544b
RicoHHN <rico.anzer@web.de>
2023-11-24 17:35:17 +0100

    Performance optimizations

--- Assets/Scripts/Cars/Pathfinding.cs
+++ Assets/Scripts/Cars/Pathfinding.cs
@@ -54,12 +54,14 @@ public class Pathfinding : MonoBehaviour
     //Calculate fastest route using A*Star Algorithm
     private void calculateRoute()
     {
-        //Spawn point of car
-        Transform origin = this.GetComponent<MoveCar>().origin;
+        //Store Skripts as variables for performance reasons
+        MoveCar thisMoveCar = this.GetComponent<MoveCar>();
 
+        //Spawn point of car
+        Transform origin = thisMoveCar.origin;
 
         //End point of car
-        Transform destination = this.GetComponent<MoveCar>().destination;
+        Transform destination = thisMoveCar.destination;
 
         //List containing all Waypoints, that weren't checked so far
         List<Transform> openSet = new List<Transform>();
@@ -78,18 +80,24 @@ public class Pathfinding : MonoBehaviour
             //currentWaypoint is first entry in openSet
             Transform currentWaypoint = openSet[0];
 
+            //Store Skripts as variables for performance reasons
+            Waypoint currentWpWaypoint = currentWaypoint.GetComponent<Waypoint>();
+
             //For every entry in openSet...
             for (int i = 1; i < openSet.Count; i++) {
 
+                //Store Skripts as variables for performance reasons
+                Waypoint openSetWaypoint = openSet[i].GetComponent<Waypoint>();
+
                 //..calculate f costs (gCost + hCost)...
-                float fCostOpenSet = openSet[i].GetComponent<Waypoint>().gCost + openSet[i].GetComponent<Waypoint>().hCost;
-                float fCostCurrent = currentWaypoint.GetComponent<Waypoint>().gCost + currentWaypoint.GetComponent<Waypoint>().hCost;
+                float fCostOpenSet = openSetWaypoint.gCost + openSetWaypoint.hCost;
+                float fCostCurrent = currentWpWaypoint.gCost + currentWpWaypoint.hCost;
 
                 //... and check distances
                 //First compare fCost (sum of distances of Waypoint to start (gCost) and Waypoint to destination (hCost))
                 //If fCost is equal => compare hCost (distance of Waypoint to destination)
                 if (fCostOpenSet < fCostCurrent || 
-                    (fCostOpenSet == fCostCurrent && openSet[i].GetComponent<Waypoint>().hCost < currentWaypoint.GetComponent<Waypoint>().hCost)) {
+                    (fCostOpenSet == fCostCurrent && openSetWaypoint.hCost < currentWpWaypoint.hCost)) {
                     currentWaypoint = openSet[i];
                 }
             }
@@ -105,7 +113,11 @@ public class Pathfinding : MonoBehaviour
             }
 
             //Check all neighbour Waypoints
+            //Annotation: currentWaypoint.GetComponent<Waypoint>() must not be replaced by currentWpWaypoint => leads to UnassignedReferenceException
             foreach (Transform neighbour in currentWaypoint.GetComponent<Waypoint>().neighbours) {
+
+                //Store Skripts as variables for performance reasons
+                Waypoint neighbourWaypoint = neighbour.GetComponent<Waypoint>();
 
                 //Temporary WaypointTree object to contain current neigbour
                 WaypointTree child = new WaypointTree();
@@ -114,14 +126,14 @@ public class Pathfinding : MonoBehaviour
                 if (closedSet.Contains(neighbour)) continue;
 
                 //Else, calculate new gCosts (distance of current Waypoint to start point)
-                float newGcost = currentWaypoint.GetComponent<Waypoint>().gCost + Vector3.Distance(currentWaypoint.localPosition, neighbour.localPosition);
+                float newGcost = currentWpWaypoint.gCost + Vector3.Distance(currentWaypoint.localPosition, neighbour.localPosition);
 
                 //If new path to neighbour Waypoint is shorter or neigbouring Waypoint is not in openSet...
-                if (newGcost < neighbour.GetComponent<Waypoint>().gCost || !openSet.Contains(neighbour)) {
+                if (newGcost < neighbourWaypoint.gCost || !openSet.Contains(neighbour)) {
 
                     //...update gCost and hCost
-                    neighbour.GetComponent<Waypoint>().gCost = newGcost;
-                    neighbour.GetComponent<Waypoint>().hCost = Vector3.Distance(neighbour.localPosition, destination.localPosition);
+                    neighbourWaypoint.gCost = newGcost;
+                    neighbourWaypoint.hCost = Vector3.Distance(neighbour.localPosition, destination.localPosition);
 
                     //Set currentWaypoint as id
                     child.id = neighbour;
--- Assets/Scripts/Cars/SpawnCar.cs
+++ Assets/Scripts/Cars/SpawnCar.cs
@@ -45,13 +45,18 @@ public class SpawnCar : MonoBehaviour
     {
         //Create a new GameObject consisting of a randomly chosen car in carCollection
         GameObject car = Instantiate(cars[Random.Range(0, cars.Length - 1)], carCollection);
-        car.name = $"Car${gameManager.GetComponent<GameManager>().carUID}";
-        gameManager.GetComponent<GameManager>().carUID++;
+
+        //Store Skript as variable for performance reasons
+        MoveCar moveCar = car.GetComponent<MoveCar>();
+        GameManager managerSkript = gameManager.GetComponent<GameManager>();
+
+        car.name = $"Car${managerSkript.carUID}";
+        managerSkript.carUID++;
         //save transform of last car that spawned
         lastCarSpawned = car.transform;
 
         //Fill MoveCar Script with first Waypoint
-        car.GetComponent<MoveCar>().origin = spawnPoint;
+        moveCar.origin = spawnPoint;
 
         //Fill MoveCar Script with destination Waypoint
         /**
@@ -59,13 +64,13 @@ public class SpawnCar : MonoBehaviour
          !!!!Currently hard coded, need change later!!!!
          -----------------------------------------------
          */
-        car.GetComponent<MoveCar>().destination = destination;
+        moveCar.destination = destination;
 
-        car.GetComponent<MoveCar>().gameManager = gameManager;
+        moveCar.gameManager = gameManager;
 
-        car.GetComponent<MoveCar>().initalYValue = car.gameObject.transform.position.y;
+        moveCar.initalYValue = car.gameObject.transform.position.y;
 
-        gameManager.GetComponent<GameManager>().currentCars++;
+        managerSkript.currentCars++;
 
         //Place cars to spawn point
         car.gameObject.transform.position = car.gameObject.transform.position+spawnPoint.transform.position;
@@ -75,7 +80,7 @@ public class SpawnCar : MonoBehaviour
     {
         //Initialize currentTime as spawnCountdown
         currentTime = spawnCountdown;
-        maxCars = GameObject.Find("GameManager").GetComponent<GameManager>().maxCars;
+        maxCars = gameManager.GetComponent<GameManager>().maxCars;
         StartCoroutine(spawnCarAfterTime());
     }
 
